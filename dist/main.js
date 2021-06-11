0fb153538884a91fec65e6741963c7f4bbe4fa3e
jasneljuillet <jasneljuillet1995@gmail.com>
2021-06-11 09:58:23 -0400

    void, any, unknown,never and assertion

--- dist/main.js
+++ dist/main.js
@@ -71,4 +71,27 @@
         let foo: any = "wooo";
         console.log(foo.something);
 
+        //never function with never can't be executed to the end/ never is something will never append
+        const doSomething = (): never => {
+            throw "Never";
+        }
+
+        //unknown
+
+        let ageAny: any = 10;
+        let ageUnknown: unknown = 24;
+
+        let s1: string = ageAny; //no error because any desable typescript check
+        let s2: string = ageUnknown; //error because we can't assign unknown directly in other type
+
+        //assertion "as" makes type assertion who allow us to convert type in other type
+        let ageAny: any = 10;
+        let ageUnknown: unknown = 24;
+
+        let s1: string = ageAny; //no error because any desable typescript check
+        let s2: string = ageUnknown as string;
+
+        let pageNumber: string = "10";
+        let numericPageNumber: number = (pageNumber as unknown) as number;
+
 */
